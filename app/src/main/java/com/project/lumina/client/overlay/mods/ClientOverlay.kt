5bf7f86f77408c47b390760938b263f3074916aa
Phoen1x <2284257190@qq.com>
2025-07-16 10:34:49 +0800

    自动上传脚本

--- app/src/main/java/com/project/lumina/client/overlay/mods/ClientOverlay.kt
+++ app/src/main/java/com/project/lumina/client/overlay/mods/ClientOverlay.kt
@@ -17,8 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color as ComposeColor
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.*
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -184,7 +183,8 @@ class ClientOverlay : OverlayWindow(), LifecycleOwner {
 
         val text = "LuminaCN${if (watermarkText.isNotBlank()) "\n$watermarkText" else ""}"
 
-        val unifontFamily = FontFamily(Font(R.font.unifont))
+val unifontFamily = FontFamily(Font(R.font.unifont))
+
 
         val defaultFamily = FontFamily.Default
 
